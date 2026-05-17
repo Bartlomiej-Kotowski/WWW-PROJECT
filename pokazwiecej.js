9f9bba5f3eec35dd653b17b75a5c44c35ba55eb8
Bartlomiej-Kotowski <95285student@pb.edu.pl>
2026-05-17 20:50:54 +0200

    {opis_zmian}

--- pokazwiecej.js
+++ pokazwiecej.js
@@ -9,3 +9,29 @@ function schowajwiecej() {
 }
 otworzwiecej.addEventListener("click", pokazwiecej)
 zamknijwiecej.addEventListener("click", schowajwiecej)
+
+//Przycisks USTAWIENIA
+const przyciskUstawienia = document.getElementById("przycisk-ustawienia");
+const menuUstawienia = document.getElementById("menu-ustawienia");
+const przyciskMotyw = document.getElementById("przycisk-motyw");
+
+function przelaczUstawienia() {
+    if (menuUstawienia.style.display === "block") {
+        menuUstawienia.style.display = "none";
+    } else {
+        menuUstawienia.style.display = "block";
+    }
+}
+
+function zmienMotyw() {
+    document.body.classList.toggle("dark-mode");
+    
+    if (document.body.classList.contains("dark-mode")) {
+        przyciskMotyw.textContent = "Motyw jasny";
+    } else {
+        przyciskMotyw.textContent = "Motyw ciemny";
+    }
+}
+
+przyciskUstawienia.addEventListener("click", przelaczUstawienia);
+przyciskMotyw.addEventListener("click", zmienMotyw);
