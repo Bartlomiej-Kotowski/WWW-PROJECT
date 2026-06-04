dd067af961985eeaad52cba7cfac8196dd5a520f
Igor <igor10071@wp.pl>
2026-06-04 23:06:54 +0200

    Upiększono FAQ i zrobiono 'O nas'

--- main.js
+++ main.js
@@ -241,7 +241,47 @@ onas = document.createElement('div');
 onas.id = 'onas';
 onas.className = 'strona';
 onas.style.display = 'none';
-onas.textContent = 'placeholder o nas';
+const daneZespolu = [
+    {
+        imie: "Kamil",
+        rola: "API / Logika strony",
+        opis: "Podpiął stronę pod API, żeby wyświetlała prawdziwą pogodę i prognozy. Zrobił też system podstron i upewnił się, że wszystko dobrze wygląda na ekranach telefonów. Zadbał również o strukturę kodu."
+    },
+    {
+        imie: "Bartłomiej",
+        rola: "CSS / Wygląd i animacje",
+        opis: "Zajął się tym, żeby strona po prostu dobrze wyglądała. Zrobił od zera ruchome tło z chmurami, ogarnął działający tryb ciemny i poukładał cały kod CSS, żeby był w nim porządek."
+    },
+    {
+        imie: "Igor",
+        rola: "Budowa elementów strony",
+        opis: "Zbudował główne przyciski na stronie głównej i stworzył od podstaw całą zakładkę 'FAQ' i 'O nas' – ułożył kafelki, wymyślił pytania i zadbał o to, żeby fajnie się to czytało."
+    }
+];
+
+// Tworzymy główny pojemnik na kafelki wizytówek
+const kontenerZespolu = document.createElement('div');
+kontenerZespolu.className = 'kontener-zespolu';
+
+
+daneZespolu.forEach(osoba => {
+    const karta = document.createElement('div');
+    karta.className = 'kafelek-osoba';
+
+    const imieElement = document.createElement('h2');
+    imieElement.textContent = osoba.imie;
+
+    const rolaElement = document.createElement('h4');
+    rolaElement.textContent = osoba.rola;
+
+    const opisElement = document.createElement('p');
+    opisElement.textContent = osoba.opis;
+
+    karta.append(imieElement, rolaElement, opisElement);
+    kontenerZespolu.append(karta);
+});
+onas.append(kontenerZespolu);
+
 document.body.append(onas);
 
 const faq = document.createElement('div');
